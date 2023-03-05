7e5d0e522b571960c1cc1bd7c140efea059f21fa
liujianjie <1219277604@qq.com>
2023-03-05 14:58:39 +0800

    017018

--- premake5.lua
+++ premake5.lua
@@ -5,6 +5,9 @@ workspace "Hazel"		-- sln文件名
 		"Release",
 		"Dist"
 	}
+	-- 启动项目
+	startproject "Sandbox"
+
 -- https://github.com/premake/premake-core/wiki/Tokens#value-tokens
 -- 组成输出目录:Debug-windows-x86_64
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
@@ -19,12 +22,17 @@ include "Hazel/vendor/GLFW"
 include "Hazel/vendor/Glad"
 include "Hazel/vendor/imgui"
 
+
+
 project "Hazel"		--Hazel项目
 	location "Hazel"--在sln所属文件夹下的Hazel文件夹
 	kind "SharedLib"--dll动态库
 	language "C++"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}") -- 输出目录
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")-- 中间目录
+	-- On:代码生成的运行库选项是MTD,静态链接MSVCRT.lib库;
+	-- Off:代码生成的运行库选项是MDD,动态链接MSVCRT.dll库;打包后的exe放到另一台电脑上若无这个dll会报错
+	staticruntime "Off"	
 
 	-- 预编译头 
 	pchheader "hzpch.h"
@@ -53,15 +61,12 @@ project "Hazel"		--Hazel项目
 	-- 如果是window系统
 	filter "system:windows"
 		cppdialect "C++17"
-		-- On:代码生成的运行库选项是MTD,静态链接MSVCRT.lib库;
-		-- Off:代码生成的运行库选项是MDD,动态链接MSVCRT.dll库;打包后的exe放到另一台电脑上若无这个dll会报错
-		staticruntime "On"	
+		--staticruntime "On"
 		systemversion "latest"	-- windowSDK版本
 		-- 预处理器定义
 		defines{
 			"HZ_PLATFORM_WINDOWS",
 			"HZ_BUILD_DLL",
-			"HZ_ENABLE_ASSERTS",
 			"GLFW_INCLUDE_NONE" -- 让GLFW不包含OpenGL
 		}
 		-- 编译好后移动Hazel.dll文件到Sandbox文件夹下
@@ -71,23 +76,24 @@ project "Hazel"		--Hazel项目
 	-- 不同配置下的预定义不同
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
+	staticruntime "Off"	
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -108,24 +114,24 @@ project "Sandbox"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
 
 		defines{
 			"HZ_PLATFORM_WINDOWS"
 		}
 
+	-- 不同配置下的预定义不同
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HZ_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HZ_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
